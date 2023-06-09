8c3797862806cf46c52f58971ed14c18de950092
SmithOxenford <127893271+SmithOxenford@users.noreply.github.com>
2023-06-09 18:22:47 -0500

    Update frase-piropos.js

--- plugins/frase-piropos.js
+++ plugins/frase-piropos.js
@@ -2,7 +2,7 @@
 
 let handler = async (m, { conn, text}) => {
 
-m.reply(`╭┄〔 *${wm}* 〕┄⊱\n┊\nდ *"${pickRandom(global.piropo)}"*\n┊\n*╰━━━⊰ 𝙏𝙃𝙀 𝙈𝘼𝙆𝘼𝙉𝘼𝙆𝙔 - 𝘽𝙊𝙏 ⊱━━━━დ*`)
+m.reply(`╭┄〔 𝙿𝙸𝚁𝙾𝙿𝙾 🧐🎩🍷 〕┄⊱\n┊\nდ *"${pickRandom(global.piropo)}"*\n┊\n*╰━━━⊰ 𝚃𝙷𝙴 𝙼𝙰𝙺𝙰𝙽𝙰𝙺𝚈 - 🤖 ⊱━━━━დ*`)
 }
 handler.tags = ['frases']
 handler.command = ['piropo']
