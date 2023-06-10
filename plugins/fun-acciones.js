23f1172384da4cabc2fdcef47c347bae695e8d37
SmithOxenford <127893271+SmithOxenford@users.noreply.github.com>
2023-06-10 17:19:20 -0500

    Update fun-acciones.js

--- plugins/fun-acciones.js
+++ plugins/fun-acciones.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, groupMetadata, text, command}) => {
-  if (!m.mentionedJid[0] && !m.quoted) throw 'ETIQUETAS A ALGUIEN DEL GRUPO PARA HACERLE ALGUNAS ACCION'
+  if (!m.mentionedJid[0] && !m.quoted) throw '𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂 𝙰 𝙰𝙻𝙶𝚄𝙸𝙴𝙽 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 𝙿𝙰𝚁𝙰 𝙷𝙰𝙲𝙴𝚁𝙻𝙴 𝙰𝙻𝙶𝚄𝙽𝙰𝚂 𝙰𝙲𝙲𝙸𝙾𝙽'
   let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let participants = groupMetadata.participants
-  conn.reply(m.chat, `Le acabas de ${command} ${text} a *@${user.split('@')[0]}* 😳`, null, { mentions: [user] })
+  conn.reply(m.chat, `𝙻𝙴 𝙰𝙲𝙰𝙱𝙰𝚂 𝙳𝙴 ${command} ${text} 𝙰 *@${user.split('@')[0]}* 😳`, null, { mentions: [user] })
 
 }
 handler.help = ['acciones']
