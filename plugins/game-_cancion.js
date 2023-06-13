b6c6b43df2d7bf1aa456ab6e5c40cd4a5233ac24
SmithOxenford <127893271+SmithOxenford@users.noreply.github.com>
2023-06-13 18:46:31 -0500

    Update game-_cancion.js

--- plugins/game-_cancion.js
+++ plugins/game-_cancion.js
@@ -3,18 +3,18 @@ const threshold = 0.72
 let handler = {
 async before(m) {
 let id = m.chat
-if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/ADIVINA EL TITULO DE LA CANCION/i.test(m.quoted.text)) return !0
+if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/𝙰𝙳𝙸𝚅𝙸𝙽𝙰 𝙴𝙻 𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽/i.test(m.quoted.text)) return !0
 this.tebaklagu = this.tebaklagu ? this.tebaklagu : {}
-if (!(id in this.tebaklagu)) return m.reply('El juego ha terminado')
+if (!(id in this.tebaklagu)) return m.reply('𝙴𝙻 𝙹𝚄𝙴𝙶𝙾 𝙷𝙰 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙳𝙾 𝙿𝙸𝙿𝙸𝙿𝙸 ☹️')
 if (m.quoted.id == this.tebaklagu[id][0].id) {
 let json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]))
 if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
 global.db.data.users[m.sender].exp += this.tebaklagu[id][2]
-m.reply(`✅Correcto!\n+${this.tebaklagu[id][2]} XP`)
+m.reply(`✅𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾!\n+${this.tebaklagu[id][2]} XP`)
 clearTimeout(this.tebaklagu[id][3])
 delete this.tebaklagu[id]
 } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Casii!`)
-else m.reply(`❌Incorrecto!`)
+else m.reply(`❌𝙸𝙽𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾!`)
 }
 return !0
 },
