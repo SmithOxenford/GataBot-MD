4d7ef77c56d2dc77f023dc4dd9574ff45e1efd1b
SmithOxenford <127893271+SmithOxenford@users.noreply.github.com>
2023-06-14 17:22:49 -0500

    Update buscador-playstore.js

--- plugins/buscador-playstore.js
+++ plugins/buscador-playstore.js
@@ -1,22 +1,22 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, args }) => {
-	if (!args[0]) throw `${lenguajeGB['smsAvisoMG']()}𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙡 𝙣𝙤𝙢𝙗𝙧𝙚 𝙙𝙚 𝙡𝙖 𝙖𝙠𝙥 𝙥𝙖𝙧𝙖 𝙗𝙪𝙨𝙘𝙖𝙧`
+	if (!args[0]) throw `${lenguajeGB['smsAvisoMG']()}𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝙿𝙰𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁 🔍`
 	let enc = encodeURIComponent(text)
 try {
 let json = await fetch(`https://latam-api.vercel.app/api/playstore?apikey=brunosobrino&q=${enc}`)
 let gPlay = await json.json()
 if (!gPlay.titulo) return m.reply(`[ ! ] Sin resultados`)
-conn.sendMessage(m.chat,{image:{url: gPlay.imagen},caption:`🔍 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊𝙎: ${gPlay.titulo}
-🧬 𝙄𝘿𝙀𝙉𝙏𝙄𝙁𝙄𝘾𝘼𝘿𝙊𝙍: ${gPlay.id}
-⛓️ 𝙇𝙄𝙉𝙆: ${gPlay.link}
-🖼️ 𝙄𝙈𝘼𝙂𝙀𝙉: ${gPlay.imagen}
-✍️ 𝘿𝙀𝙎𝘼𝙍𝙍𝙊𝙇𝙇𝘼𝘿𝙊𝙍: ${gPlay.desarrollador}
-📜 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝘾𝙄𝙊́𝙉: ${gPlay.descripcion}
-💲 𝙈𝙊𝙉𝙀𝘿𝘼: ${gPlay.moneda}
-🎭 𝙂𝙍𝘼𝙏𝙄𝙎?: ${gPlay.gratis}
-💸 𝙋𝙍𝙀𝘾𝙄𝙊: ${gPlay.precio}
-📈 𝙋𝙐𝙉𝙏𝙐𝘼𝘾𝙄𝙊́𝙉: ${gPlay.puntuacion}`},{quoted:m})
+conn.sendMessage(m.chat,{image:{url: gPlay.imagen},caption:`🔍 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 : ${gPlay.titulo}
+🧬 𝐈𝐃𝐄𝐍𝐓𝐈𝐅𝐈𝐂𝐀𝐃𝐎𝐑 : ${gPlay.id}
+⛓️ 𝐋𝐈𝐍𝐊 : ${gPlay.link}
+🖼️ 𝐈𝐌𝐀𝐆𝐄𝐍 : ${gPlay.imagen}
+✍️ 𝐃𝐄𝐒𝐀𝐑𝐑𝐎𝐋𝐋𝐀𝐃𝐎𝐑 : ${gPlay.desarrollador}
+📜 𝐃𝐄𝐒𝐂𝐑𝐈𝐏𝐂𝐈𝐎𝐍 : ${gPlay.descripcion}
+💲 𝐌𝐎𝐍𝐄𝐃𝐀 : ${gPlay.moneda}
+🎭 𝐆𝐑𝐀𝐓𝐈𝐒? : ${gPlay.gratis}
+💸 𝐏𝐑𝐄𝐂𝐈𝐎 : ${gPlay.precio}
+📈 𝐏𝐔𝐍𝐓𝐔𝐀𝐂𝐈𝐎𝐍 : ${gPlay.puntuacion}`},{quoted:m})
 } catch (e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
