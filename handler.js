222cbe3d1bc41629c8b784f2abb06ace6a708fc0
SmithOxenford <127893271+SmithOxenford@users.noreply.github.com>
2023-06-26 18:54:34 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1554,7 +1554,7 @@ let msg = {
 }[type]
 //if (msg) return m.reply(msg)
 let tg = { quoted: m, userJid: conn.user.jid }
-let prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: msg, contextInfo: { externalAdReply: { title: lenguajeGB.smsAvisoAG().slice(0,-2), body: [wm, '🤖 𝐒𝐔𝐏𝐄𝐑 ' + gt + ' ', '🌟 botmakanaky.gmail.com'].getRandom(), thumbnail: gataImg.getRandom(), sourceUrl: [md, nna, nn, nnn, nnnt, nnnttt4, yt, ig, paypal, fb].getRandom() }}}}, tg)
+let prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: msg, }}, tg)
 if (msg) return conn.relayMessage(m.chat, prep.message, { messageId: prep.key.id })
 }
 
