c3b79423f4124e5410d8a401c2720d70ff0a2801
SmithOxenford <127893271+SmithOxenford@users.noreply.github.com>
2023-06-28 12:53:10 -0500

    Update rpg-role.js

--- plugins/rpg-role.js
+++ plugins/rpg-role.js
@@ -84,7 +84,7 @@ const roles = {
 export function before(m) {
         let user = db.data.users[m.sender]
         let level = user.level
-        let role = (Object.entries(roles).sort((a, b) => b[1] - a[1]).f𝙸nd(([, minLevel]) => level >= minLevel) || Object.entries(roles)[0])[0]
+        let role = (Object.entries(roles).sort((a, b) => b[1] - a[1]).find(([, minLevel]) => level >= minLevel) || Object.entries(roles)[0])[0]
         user.role = role
         return !0
     
